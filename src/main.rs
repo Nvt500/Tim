bce74e99a22d7690f02deab18694c0161e509069
Nvt5 <121981147+Nvt500@users.noreply.github.com>
2024-08-20 17:38:08 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main()
 {
     let args: Vec<String> = env::args().collect();
 
-    let config = tim::Config::build(&args).unwrap_or_else(|err| {
+    let config = tim_cli::Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}.");
         process::exit(1);
     });
 
-    if let Err(err) = tim::run(config)
+    if let Err(err) = tim_cli::run(config)
     {
         eprintln!("{err}");
         process::exit(1);
